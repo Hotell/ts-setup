3e9e2739ff91e920193d537964c69cda3b7f8650
Martin Hochel <hochelmartin@gmail.com>
2018-12-19 12:16:31 +0100

    build(ts-config): make build generic

--- packages/ts-config/scripts/build-tsconfig.js
+++ packages/ts-config/scripts/build-tsconfig.js
@@ -1,10 +1,44 @@
 // @ts-check
 const { copyFileSync } = require('fs')
 const { resolve } = require('path')
-// const template = require('../src/tsconfig.lib.json')
 
-const tsconfigTemplate = resolve(__dirname, '..', 'src', 'tsconfig.lib.json')
-const dist = resolve(__dirname, '..', 'dist')
-const finalTsconfig = resolve(dist, 'tsconfig.json')
+const args = process.argv.slice(2)
 
-copyFileSync(tsconfigTemplate, finalTsconfig)
+const ROOT = resolve(__dirname, '..')
+const dist = resolve(ROOT, 'dist')
+
+main()
+
+function main() {
+  const { template } = parseArgs(args)
+  createTSconfig({ templateName: template })
+}
+
+// =============================================================================
+// helpers
+// =============================================================================
+
+/**
+ *
+ * @param {string[]} args
+ */
+function parseArgs(args) {
+  if (args.length === 0 || args.length > 1) {
+    throw new Error('only valid argument is path to template')
+  }
+
+  return {
+    template: args[0],
+  }
+}
+
+/**
+ *
+ * @param {{templateName:string,configFileName?:string}} args
+ */
+function createTSconfig({ templateName, configFileName = 'tsconfig.json' }) {
+  const tsconfigTemplate = resolve(ROOT, templateName)
+  const finalTsconfig = resolve(dist, configFileName)
+
+  copyFileSync(tsconfigTemplate, finalTsconfig)
+}
